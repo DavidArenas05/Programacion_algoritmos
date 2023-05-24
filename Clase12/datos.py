ba0e7e2a748dcd35f08c639003559d5f9c26650c
DavidArenas05 <134552802+DavidArenas05@users.noreply.github.com>
2023-05-24 16:43:22 -0400

    datos_solicitados

---
+++ Clase12/datos.py
@@ -0,0 +1,9 @@
+#EJEMPLO DE USO DE GITHUB
+print ("INGRESO DE DATOS: ")
+print ()
+vnom=input("Ingrese su nombre: ")
+vedad=input("Ingrese su edad: ")
+print ()
+print (f"Su nombre es: {vnom}")
+print (f"Su edad es: {vedad}")
+print ("PROGRAMA FINALIZADO")
